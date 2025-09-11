dd1f4b286711c35505cafa14186537849064cadb
lud <ludovic@demblans.com>
2025-09-11 07:19:08 +0200

    chore: Setup mcp jest tool for later

---
+++ test/gen_mcp/mcp_jest_test.exs
@@ -0,0 +1,18 @@
+defmodule GenMcp.McpJestTest do
+  import GenMcp.Test.Client
+  use ExUnit.Case, async: true
+
+  defp basic_path do
+    "/mcp/basic"
+  end
+
+  @tag :skip
+  test "execute Calculator" do
+    assert {_, 0} =
+             System.cmd(
+               "npx",
+               ~w(mcp-jest --transport streamable-http --url #{url(basic_path())} --tools Calculator),
+               into: IO.stream()
+             )
+  end
+end
