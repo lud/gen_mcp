6dc35d723db92c103ef3b26fb1e76d47e40ca8e3
lud <ludovic@demblans.com>
2025-11-17 08:40:42 +0100

    new version 0.2.0

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule GenMCP.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/lud/gen_mcp"
-  @version "0.1.0"
+  @version "0.2.0"
   def project do
     [
       app: :gen_mcp,
